9fd446a8c1941c2eb88ade874df8a47b0088a599
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-04 04:19:32 +0600

    grade calculate with if else statement

---
+++ 35-gradeCalculator.js
@@ -0,0 +1,26 @@
+function gradeCalculator(number){
+    if(number > 80){
+        return "You got A+";
+    }
+    else if(number > 70){
+        return "You got A";
+    }
+    else if(number > 60){
+        return "You got A-";
+    }
+    else if(number > 50){
+        return "You got B";
+    }
+    else if(number > 40) {
+        return "You got C";
+    }
+    else if(number > 33) {
+        return "You got D";
+    }
+    else{
+        return "Oops! You are fail.."
+    }
+}
+const myNumber = 70; 
+const myGrade = gradeCalculator(myNumber);
+console.log(myGrade);
