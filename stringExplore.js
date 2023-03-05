dfb937d734e755beff33ece043ccbfaef5a2e570
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-05 09:16:14 +0600

    string comparison with toUpperCase and toLowerCase

--- stringExplore.js
+++ stringExplore.js
@@ -5,4 +5,14 @@ let myAddress = 'Shekhati';
 myAddress = myAddress + ' ' + 'Jessore';
 // console.log(myAddress);
 
+// string comparison with toUpperCase() and toLowerCase()
+const userName = 'Rony';
+const userInput = 'rony';
+if(userName.toUpperCase === userInput.toUpperCase){
+    console.log('Valid User');
+}
+else {
+    console.log('Invalid User');
+}
+
 
