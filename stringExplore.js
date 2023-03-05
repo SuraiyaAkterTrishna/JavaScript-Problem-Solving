e4cc42c58e6ba0fa09b318298990ae45d81adacc
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-05 08:41:54 +0600

    string cannot update but it can be changed

--- stringExplore.js
+++ stringExplore.js
@@ -1,3 +1,6 @@
 const myName = 'Suraiya';
 // console.log(myName.length);
-console.log(myName[3]);
+// console.log(myName[3]);
+let myAddress = 'Shekhati';
+myAddress = myAddress + ' ' + 'Jessore';
+console.log(myAddress);
