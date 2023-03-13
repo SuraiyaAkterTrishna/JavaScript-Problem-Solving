7881d973892e180e8d4024eb66a94b465823a904
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-13 23:17:41 +0600

    remove duplicate elements from array

---
+++ 42-remove-duplicate-from-array.js
@@ -0,0 +1,27 @@
+const names = ['abul', 'babul', 'cabul', 'dabul', 'kabul', 'babul'];
+function removeDuplicate(names){
+    let unique = [];
+    for(let i = 0; i < names.length; i++){
+        const name = names[i];
+        if(unique.includes(name)=== false){
+            unique.push(name);
+        }
+    }
+    return unique;
+}
+const uniqueNames = removeDuplicate(names);
+console.log(uniqueNames);
+
+const numbers = [51, 41, 61, 51, 21, 41, 31, 61, 61, 71, 61, 71, 81, 31];
+function removeDuplicateNumbers(numbers){
+    let uniqueNumbers = [];
+    for(let i = 0; i < numbers.length; i++){
+        const number = numbers[i];
+        if(uniqueNumbers.includes(number) === false){
+            uniqueNumbers.push(number);
+        }
+    }
+    return uniqueNumbers;
+}
+const uniqueNumbersFromArray = removeDuplicateNumbers(numbers);
+console.log(uniqueNumbersFromArray);
