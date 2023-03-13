d1c043291dbd2b10887a62f468ab0c5330d72b75
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-14 03:14:12 +0600

    wood calculator

---
+++ 44-woodCalculator.js
@@ -0,0 +1,14 @@
+function woodCalculator(chairQuantity, tableQuantity, bedQuantity){
+    const perChairWood = 3; 
+    const perTableWood = 10; 
+    const perBedWood = 50; 
+
+    const chairWood = chairQuantity * perChairWood;
+    const tableWood = tableQuantity * perTableWood;
+    const bedWood = bedQuantity * perBedWood;
+
+    const totalWood = chairWood + tableWood + bedWood;
+    return totalWood;
+}
+const furniture = woodCalculator(2, 2, 5);
+console.log(furniture);
