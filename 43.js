af90fd1bd5fd6e08683b5ed013907b8c6a29dd42
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-14 00:34:24 +0600

    43 problem solve done

---
+++ 43.js
@@ -0,0 +1,20 @@
+/* 
+1. show output from 1-50
+2. if the number is divisible by 3 then instead of the number show 'foo'
+3. if the number is divisible by 5 then instead of the number show 'bar'
+4. if the number is divisible by both 3 and 5 then instead of the number show 'foobar'
+*/
+
+for(let i = 1; i <= 50; i++){
+    let number = i;
+    if((number % 3 === 0) && (number % 5 === 0)){
+        number = 'foobar';
+    }
+    if(number % 3 === 0){
+        number = 'foo';
+    }
+    if(number % 5 === 0){
+        number = 'bar';
+    }
+    console.log(number);
+}
