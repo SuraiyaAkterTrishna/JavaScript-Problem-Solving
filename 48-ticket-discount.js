c84abe669d68a4f85bd94d8ef3ae7f8197c9b993
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-14 20:09:49 +0600

    ticket requirement

---
+++ 48-ticket-discount.js
@@ -0,0 +1,9 @@
+/* 
+1. if ticket numbers is less than 100, per ticket price: 100
+2. if ticket numbers is more than 100, but less than 200. First 100 tickets will be 100/ticket,
+rest tickets will be 90 taka per price
+3. if you purchase more than 200 ticket, per ticket price: 70tk
+    first 100   ---> 100tk
+    101-200     ---> 90tk
+    200+        ---> 70tk
+*/
