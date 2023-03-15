2f903e2bd9382509504fc9e35a980527db8cf9fc
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-15 19:07:10 +0600

    add sub multiply division

--- 50-checklist.js
+++ 50-checklist.js
@@ -3,7 +3,7 @@ const friend = 'Rozy';
 const age = 45; 
 const isBlack = false;
 
-console.log(friend, age, isBlack);
+// console.log(friend, age, isBlack);
 
 // let const 
 const myAddress = "Rupsa";
@@ -14,4 +14,20 @@ const myAddress = "Rupsa";
 let mySalary = '300 usd';
 // let mySalary = '200 usd'; //redefine will give error
 mySalary = '500 usd'; //but reassign possible
-console.log(mySalary);
+// console.log(mySalary);
+
+const rabbi = 50; 
+const sagor = 100; 
+
+const total = rabbi + sagor; 
+// console.log(total);
+
+const moreMoney = sagor - rabbi;
+// console.log(moreMoney);
+
+const multiplyMoney = rabbi * sagor;
+// console.log(multiplyMoney);
+
+const divineMoney = sagor / rabbi;
+// console.log(divineMoney);
+
