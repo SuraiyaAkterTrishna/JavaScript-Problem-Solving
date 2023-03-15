e3d7753d3d0a7781b82fc276b909f118774b2c7f
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-15 19:51:56 +0600

    centimeter to meter

--- 52-centimeterToMeter.js
+++ 52-centimeterToMeter.js
@@ -1,5 +1,6 @@
 function centimeterToMeter(centimeters){
-    const meters = centimeters * 100; 
+    const meters = centimeters / 100; 
     return meters;
 }
 const road = centimeterToMeter(5000);
+console.log(road);
