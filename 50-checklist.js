69752bd9de3d3f38ce44143cef196855b2e90e7b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-03-15 19:01:44 +0600

    checklist variable declaration

--- 50-checklist.js
+++ 50-checklist.js
@@ -4,3 +4,14 @@ const age = 45;
 const isBlack = false;
 
 console.log(friend, age, isBlack);
+
+// let const 
+const myAddress = "Rupsa";
+// myAddress = "Khulna"; //reassign will give error
+
+// console.log(myAddress);
+
+let mySalary = '300 usd';
+// let mySalary = '200 usd'; //redefine will give error
+mySalary = '500 usd'; //but reassign possible
+console.log(mySalary);
