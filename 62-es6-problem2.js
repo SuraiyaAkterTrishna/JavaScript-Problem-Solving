0d39281a481e2dc83c43db9624a4acaa57980c2b
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-28 11:24:34 +0600

    second done

---
+++ 62-es6-problem2.js
@@ -0,0 +1,12 @@
+/* 2) Write the following sentence in three lines and print the result:
+I am a web developer. I love to code. I love to eat biryani. */
+
+const first = 'I am a web developer';
+const second = 'I love to code';
+const third = 'I love to eat biryani';
+const aboutMe = `
+${first}. 
+${second}.
+${third}. 
+`;
+console.log(aboutMe);
